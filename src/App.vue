2cfda183863bf006e93c3194cc24a4e3170a0325
pierobramanti <piero.bramanti94@gmail.com>
2024-08-26 17:23:38 +0200

    [UPD] created header.vue and main.vue and inserted in App

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <script>
 import { store } from './store';
 import axios from 'axios';
+import Header from './Header.vue';
+import Main from './Main.vue';
 export default {
+  components: {
+    Header,
+    Main
+  },
   data() {
     return {
       store,
-      axios
     }
   }
 }
 </script>
 
 <template>
+<Header />
+<Main />
 </template>
 <style lang="scss">
 @use "./assets/style/generals.scss";
---
+++ src/Header.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    
+}
+</script>
+<template>
+    <div class="container-fluid">
+        <input type="text" placeholder="Search">
+        <button>Cerca</button>
+    </div>
+</template>
+<style lang="scss">
+</style>
